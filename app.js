7e00d7a0905136884d267de22288491e332288b6
Tulloch022 <90489018+tulloch022@users.noreply.github.com>
2022-03-08 17:56:22 -0800

    fixed alternating colors of table

--- app.js
+++ app.js
@@ -26,10 +26,16 @@ class UI {
         row.innerHTML = `
         <td>${record.title}</td>
         <td>${record.artist}</td>
-        <td><a href="#">X</a></td>
+        <td><a class="delete" href="#">X</a></td>
         `;
 
         list.appendChild(row);
+    }
+
+    static deleteRecord(el) {
+        if (el.classList.contains('delete')) {
+            el.parentElement.parentElement.remove();
+        }
     }
 
     static clearFields() {
@@ -52,13 +58,23 @@ document.querySelector('#vinyl-form').addEventListener('submit', (e) => {
     const title = document.querySelector('#title').value;
     const artist = document.querySelector('#artist').value;
 
+    if (title === '' || artist === '') {
+        alert('Please fill in all fields')
+    } else {
+
+
     const record = new Record(title, artist);
 
     UI.addRecordToList(record);
 
     UI.clearFields()
+    }
 });
 
 
 
 //Event: Remove a Record
+
+document.querySelector("#record-list").addEventListener('click', (e) => {
+   UI.deleteRecord(e.target)
+})
