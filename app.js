d00472d44a90d8cfaf4c719c933b8de705cd4a26
Tulloch022 <90489018+tulloch022@users.noreply.github.com>
2022-03-08 16:57:09 -0800

    script to add to list

---
+++ app.js
@@ -0,0 +1,57 @@
+//Record Class: Represents a Record
+
+class Record {
+    constructor(title, artist) {
+        this.title = title;
+        this.artist = artist;
+    }
+}
+
+//UI Class: Handle UI Tasks
+
+class UI {
+    static displayRecords() {
+        const storedRecords = [];
+
+        const records = storedRecords;
+
+        records.forEach((record) => UI.addRecordToList(record));
+    }
+
+    static addRecordToList(record) {
+        const list = document.querySelector('#record-list');
+
+        const row = document.createElement('tr');
+
+        row.innerHTML = `
+        <td>${record.title}</td>
+        <td>${record.artist}</td>
+        <td><a href="#">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+//Store Class: Handles Storage
+
+//Event: Display Records
+
+document.addEventListener('DOMContentLoaded', UI.displayRecords);
+
+//Event: Add a Record
+
+document.querySelector('#vinyl-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    const title = document.querySelector('#title').value;
+    const artist = document.querySelector('#artist').value;
+
+    const record = new Record(title, artist);
+
+    UI.addRecordToList(record);
+})
+
+
+
+//Event: Remove a Record
